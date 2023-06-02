5d0809f251337169b4d0d2f5aab3205dc41fed2c
Filipe <fbianchi.andrade@gmail.com>
2023-06-02 17:17:27 -0400

    tentando fazer o jogo com input

--- js/script2.js
+++ js/script2.js
@@ -1,7 +1,7 @@
 const numero = Math.floor(Math.random() * (9 - 0 + 1) + 0);
 console.log(numero);
 
-verificaAcerto ();
+//verificaAcerto();
 
 function verificaAcerto() {
 
@@ -15,9 +15,28 @@ function verificaAcerto() {
             break;
         }
     }
-    if (chute == numero){
+    if (chute == numero) {
         alert(`Parabéns, você acertou! O número correto era ${numero}.`)
     } else {
         alert(`Fim de jogo, o número correto era ${numero}.`)
     }
 }
+
+
+    document.querySelector('.botao').addEventListener('click', () => {
+        var numeroInserido = document.querySelector('#numero');
+        var texto = numeroInserido.value;
+        console.log(texto)
+
+        for (i = 0; i < 2; i++) {
+            if (texto != numero) {
+                document.querySelector('.resposta'). innerHTML = 'Você errou!';
+                alert('Tente novamente.');
+            } else {
+                document.querySelector('.resposta'). innerHTML = 'Você acertou!'
+                break;
+            }
+        }
+
+    })
+
