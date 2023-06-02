ff09171cc3f4ab186678e67640c267d575cc0e72
Filipe <fbianchi.andrade@gmail.com>
2023-06-02 16:17:59 -0400

    adicionando mais uma verificação após o loop

--- js/script2.js
+++ js/script2.js
@@ -7,14 +7,18 @@ function verificaAcerto() {
 
     let chute = prompt('Digite um número.');
 
-    for (i = 0; i < 2; i++) {
+    for (i = 0; i < 3; i++) {
         if (chute != numero) {
             alert('errou');
-            verificaAcerto ();
-            break;
+            chute = prompt('tente novamente.')
         } else {
             alert('Parabéns, você acertou.');
             break;
         }
+    }
+    if (chute == numero){
+        alert(`Parabéns, você acertou! O número correto era ${numero}.`)
+    } else {
+        alert(`Fim de jogo, o número correto era ${numero}.`)
     }
 }
