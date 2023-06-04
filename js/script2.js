d9979e12c9abbccf0fc3ae194de4fb155fb90a86
Filipe de Bianchi Andrade <fbianchi.andrade@gmail.com>
2023-06-04 14:37:49 -0300

    criando branch desenvolvimento2

--- js/script2.js
+++ js/script2.js
@@ -7,19 +7,15 @@ document.querySelector('.botao').addEventListener('click', () => {
     var numeroInserido = document.querySelector('#numero');
     let texto = parseInt(numeroInserido.value);
     console.log(texto);
-    
-    
-    for (i = 2; i > 0; i--) {
 
         if (texto == numero) {
             resposta.innerHTML = 'Você acertou!';
-            break;
 
         } else {
-            resposta.innerHTML = `Você errou! Você tem mais ${i} tentativas`;
+            while (texto != numero) {
+            resposta.innerHTML = `Você errou!`;
             document.getElementById('numero').value = "";
             break;
-
+            }
         }
-    }
 });
